38568b260e56bec25ce8d76900a1601974edac5c
Diego <diegoospinarojas7-droid@users.noreply.github.com>
2026-07-24 14:03:48 -0500

    aplicacion express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    res.send('Aprendices ficha 3407186');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor en funcionamiento en el puerto:`);
+});
+
